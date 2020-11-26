58136466cc528e31ee0e33533950fc34358ea2c0
rodrigo-rpds95 <rodrigo.rpds95@gmail.com>
2020-11-25 23:17:38 -0300

    fix header link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Header from './components/Header';
-import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Footer />
     </div>
   );
 }
