e0c67299aa34794d04364290d4bd70cfd6580b7c
rodrigo-rpds95 <rodrigo.rpds95@gmail.com>
2020-12-09 23:29:18 -0300

    update style menu profile

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import './Header.css';
 
 const Header = () => {
 
-    const [expand, setExpandInput] = useState(false);
+    const [expandedInput, setExpandSearchInput] = useState(false);
 
     function onClickHandler(){
-        (window.innerWidth < 1100) ? setExpandInput(true) : setExpandInput(false);
+        (window.innerWidth < 1100) ? setExpandSearchInput(true) : setExpandSearchInput(false);
     }
 
     return (
@@ -20,29 +20,29 @@ const Header = () => {
                     <li><a href="/">Infantil</a></li>
                     <li><a href="/">Canais</a></li>
                 </ul>
-                <div className={expand ? 'header-search expand' : 'header-search'}>
+                <div className={expandedInput ? 'header-search expandedInput' : 'header-search'}>
                     <input onClick={onClickHandler} type="search" name="search" placeholder="Busca" />
-                    <span className="close-search" onClick={e => setExpandInput(false)}></span>
+                    <span className="close-search" onClick={e => setExpandSearchInput(false)}></span>
                 </div>
                 <div className="header-profile">
                     <img src="https://m.media-amazon.com/images/G/02/CerberusPrimeVideo-FN38FSBD/adult-1.png" alt="Profile" />
                     <span className="profile-name">User</span>
                 </div>
-            </div>
-            <div className="menu-header">
-                <ul>
-                    <li><a href="/">Sua Lista</a></li>
-                    <li><a href="/">Conta e configurações</a></li>
-                    <li><a href="/">Assista onde quiser</a></li>
-                    <li><a href="/">Ajuda</a></li>
-                    <li><a href="/">Não é Rodrigo? Sair</a></li>
-                </ul>
-                <ul>
-                    <li><a href="/">crianças</a></li>
-                    <li><a href="/">Adicionar novo perfil</a></li>
-                    <li><a href="/">Editar perfis</a></li>
-                    <li><a href="/">Saiba mais</a></li>
-                </ul>
+                <div className="menu-header">
+                    <ul>
+                        <li><a href="/">crianças</a></li>
+                        <li><a href="/">Adicionar novo perfil</a></li>
+                        <li><a href="/">Editar perfis</a></li>
+                        <li><a href="/">Saiba mais</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="/">Sua Lista</a></li>
+                        <li><a href="/">Conta e configurações</a></li>
+                        <li><a href="/">Assista onde quiser</a></li>
+                        <li><a href="/">Ajuda</a></li>
+                        <li><a href="/">Não é Rodrigo? Sair</a></li>
+                    </ul>
+                </div>
             </div>
         </header>
     )
